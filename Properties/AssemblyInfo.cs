970c8a58877a20102d192cc2ffc7acc2098c36d8
sobelito <sobelito@gmail.com>
2015-05-13 20:40:57 +0000

    Cleaning up ignore file, added comment, nothing important
    Still a Git N00b

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+//Self-updating list of active open proxies and an example consuming/scraping sites with the dynamic list
